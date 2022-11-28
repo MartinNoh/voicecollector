95a87e361a43abd5283b76df26161ee00fec65ec
동경 <ehdrud1129@naver.com>
2022-11-28 10:53:48 +0900

    init commit

---
+++ voicecollector/src/main/java/com/donggyeong/voicecollector/VoicecollectorApplication.java
@@ -0,0 +1,13 @@
+package com.donggyeong.voicecollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoicecollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoicecollectorApplication.class, args);
+	}
+
+}
---
+++ voicecollector/src/test/java/com/donggyeong/voicecollector/VoicecollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donggyeong.voicecollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoicecollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
